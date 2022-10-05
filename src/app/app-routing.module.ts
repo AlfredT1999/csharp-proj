7633493486123017b064f65f226a0d9d0c3f9018
Alfredo Tapia <71139502+AlfredT1999@users.noreply.github.com>
2022-10-05 15:27:46 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Chapter2Component } from './chapter2/chapter2.component';
+import { Section1Component } from './chapter2/section1/section1.component';
+import { Section2Component } from './chapter2/section2/section2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: Chapter2Component
+  },
+  {
+    path: 'section1',
+    component: Section1Component
+  },
+  {
+    path: 'section2',
+    component: Section2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { Chapter2Component } from './chapter2/chapter2.component';
+import { Section1Component } from './chapter2/section1/section1.component';
+import { Section2Component } from './chapter2/section2/section2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    Chapter2Component,
+    Section1Component,
+    Section2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
